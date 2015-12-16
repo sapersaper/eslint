23fabb2bfd84f8be9a1be46720d12c6de69c691c
Lucas <lgbartroli@gmail.com>
2015-12-16 12:53:17 -0300

    Setting ESLint Rules
    Set rules

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,22 @@
-var myFnc = function(a, b, c) {
+var myFnc = function(a, b, c)
+{
+
+    // hola!
     'use strict';
     return a + b + 'arr' + c;
 };
 
 var myArr = [1, 2, 3, 4];
+var variable1 = {
+	'a1': 'a1',
+	'a2': 'a2'
+};
+var a = 1;
+a = variable1['a' + a];
 
 myFnc(1, 2, myArr);
+
+if (myArr[0] > myArr[1])
+{
+    myArr = 1;
+}
